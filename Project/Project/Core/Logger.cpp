d7d37e9d18106000422bed05677c9c2170797cdf
sw1bexx-code <sw1bexx@gmail.com>
2026-05-21 20:28:51 +0300

    Logger.cpp

--- Project/Project/Core/Logger.cpp
+++ Project/Project/Core/Logger.cpp
@@ -1,7 +1,36 @@
-// Logger.cpp
-// Implementation file for Logger.
-// Future methods:
-// - log(message)
-// - print() const
-// - clear()
-// - getHistory() const
+#include "Logger.h"
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+Logger& Logger::getInstance() noexcept {
+    static Logger instance;
+    return instance;
+}
+
+void Logger::log(const std::string& message) {
+    history_.emplace_back("[INFO] " + message);
+}
+
+void Logger::logError(const std::string& message) {
+    history_.emplace_back("[ERROR] " + message);
+}
+
+void Logger::print() const {
+    std::cout << "\n=== SYSTEM EVENT HISTORY ===" << std::endl;
+    if (history_.empty()) {
+        std::cout << "[Log history is completely empty]" << std::endl;
+        return;
+    }
+    
+    std::copy(history_.cbegin(), history_.cend(), std::ostream_iterator<std::string>(std::cout, "\n"));
+    std::cout << "============================" << std::endl;
+}
+
+void Logger::clear() noexcept {
+    history_.clear();
+}
+
+const std::list<std::string>& Logger::getHistory() const noexcept {
+    return history_;
+}
