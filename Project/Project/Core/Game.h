9d90a1b9cfa78b009f347cc0d53fe10637abba01
sw1bexx-code <sw1bexx@gmail.com>
2026-05-21 21:32:08 +0300

    Game.h

--- Project/Project/Core/Game.h
+++ Project/Project/Core/Game.h
@@ -1,13 +1,38 @@
-// Game.h
-// Main game coordinator.
-// Responsible for:
-// - showing the main menu
-// - starting a new game
-// - loading an existing game
-// - opening the class selection menu
-// - opening the main game menu
-// - connecting dungeon, shop, save/load, logger and player character
-//
-// Important:
-// - main() should only create Game and call run()
-// - no battle/shop/save logic should be written directly in main()
+#ifndef GAME_H
+#define GAME_H
+
+#include <memory>
+#include "Logger.h"
+#include "SaveSystem.h"
+
+class Character; 
+class Shop;
+class Dungeon;
+
+class Game {
+private:
+    bool m_isRunning;
+    std::unique_ptr<Character> m_player;
+    Logger m_logger;
+    SaveSystem m_saveSystem;
+
+    void showMainMenu();
+    void showGameMenu();
+    
+    void startNewGame();
+    void continueGame();
+    void selectCharacterClass();
+    
+    void goToDungeon();
+    void goToShop();
+    void restAtHome();
+    void saveGame();
+
+public:
+    Game();
+    ~Game() = default;
+
+    void run();
+};
+
+#endif 
