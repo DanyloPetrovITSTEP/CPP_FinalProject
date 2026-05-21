b76674bf5100532c7d0b4af9eb44b9060f27ca76
sw1bexx-code <sw1bexx@gmail.com>
2026-05-21 21:26:30 +0300

    Logger.cpp

--- Project/Project/Core/Logger.cpp
+++ Project/Project/Core/Logger.cpp
@@ -1,7 +1,27 @@
-// Logger.cpp
-// Implementation file for Logger.
-// Future methods:
-// - log(message)
-// - print() const
-// - clear()
-// - getHistory() const
+#include "Logger.h"
+#include <iostream>
+
+void Logger::log(std::string_view message) {
+    m_history.emplace_back(message);
+}
+
+void Logger::print() const {
+    std::cout << "\n--- GAME LOG HISTORY ---\n";
+    if (m_history.empty()) {
+        std::cout << "[Log is empty]\n";
+        return;
+    }
+    
+    for (auto it = m_history.cbegin(); it != m_history.cend(); ++it) {
+        std::cout << *it << "\n";
+    }
+    std::cout << "------------------------\n";
+}
+
+void Logger::clear() {
+    m_history.clear();
+}
+
+const std::vector<std::string>& Logger::getHistory() const {
+    return m_history;
+}
