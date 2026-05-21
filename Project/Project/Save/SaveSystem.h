e6a08112fe1a0794134ff95077e70b664fd0f00a
sw1bexx-code <sw1bexx@gmail.com>
2026-05-21 20:24:03 +0300

    Update SaveSystem.h

--- Project/Project/Save/SaveSystem.h
+++ Project/Project/Save/SaveSystem.h
@@ -1,15 +1,13 @@
-#ifndef SAVESYSTEM_H
-#define SAVESYSTEM_H
+#pragma once
 
 #include "../Characters/Character.h"
+#include "../Characters/CharacterFactory.h"
 #include <string>
 #include <memory>
 
-class SaveSystem {
+class SaveSystem final {
 public:
     static void saveGame(const std::string& filename, const Character& player);
     
-    static std::unique_ptr<Character> loadGame(const std::string& filename);
+    static std::unique_ptr<Character> loadGame(const std::string& filename, CharacterFactory& factory);
 };
-
-#endif 
