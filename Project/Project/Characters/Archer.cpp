a74f405454afee13a4fdb653eb35ce38e0e1a679
Danil <danila919danila@gmail.com>
2026-05-25 17:42:45 +0200

    Added Archer class

--- Project/Project/Characters/Archer.cpp
+++ Project/Project/Characters/Archer.cpp
@@ -1,6 +1,10 @@
-// Archer.cpp
-// Implementation file for Archer.
-// Future logic:
-// - constructor with prepared stats
-// - archer special attack
-// - class name output
+﻿#include "Archer.h"
+
+Archer::Archer(const string& name) : Character(name, 80, 12, 50) {}
+
+string Archer::getClassName() const { return "Archer"; }
+
+void Archer::specialAbility(Character& target)
+{
+	target.takeDamage(getDamage() + 5);
+}
