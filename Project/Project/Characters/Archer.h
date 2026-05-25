7b1054e5c1c5323f65436b77a3aa607d9572dcf9
Danil <danila919danila@gmail.com>
2026-05-25 17:31:55 +0200

    Added Archer class header file

--- Project/Project/Characters/Archer.h
+++ Project/Project/Characters/Archer.h
@@ -1,14 +1,13 @@
-// Archer.h
-// Archer character class.
-// Starting idea:
-// - medium health
-// - stable damage
-// - chance to hit twice or make a precise shot
-//
-// Example starting stats:
-// - 80 HP
-// - 12 damage
-//
-// Overrides:
-// - getClassName()
-// - specialAction()
+﻿#pragma once
+
+#include "Character.h"
+
+class Archer : public Character
+{
+public:
+
+	Archer(const string& name);
+
+	string getClassName() const override;
+	void specialAbility(Character& target) override;
+};
