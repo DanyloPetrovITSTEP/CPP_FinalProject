676a666ed1a63d79d43c0481286447611efd7d6f
lemonn-ops <retryver10@gmail.com>
2026-05-25 21:31:37 +0300

    Updtated dungeon

--- Project/Project/World/Dungeon.cpp
+++ Project/Project/World/Dungeon.cpp
@@ -8,56 +8,163 @@ Dungeon::Dungeon(string dName, string bName, int lvl, Logger& logger)
     : logger_(logger)
 {
     dungeonName = dName;
-    bossName = bName;
+    boss1Name = bName;
+    level = lvl;
+}
+Dungeon::Dungeon(string dName, string b1Name, string b2Name, int lvl, Logger& logger)
+    : logger_(logger)
+{
+    dungeonName = dName;
+    boss1Name = b1Name;
+    boss2Name = b2Name;
     level = lvl;
 }
 
 void Dungeon::showInfo() 
 {
     cout << "Name: " << dungeonName << "\n"
-         << "Boss: " << bossName << "\n"
+         << "Bosses: " << boss1Name << ", " << boss2Name << "\n"
          << "Level: " << level << "\n";
 }
 
 void Dungeon::showMenu()
 {
+	logger_.log("Player opened the dungeon menu.");
     int choice;
     do {
         cout << "\n--- DUNGEON MENU ---\n"
-            << "1. Easy dungeon\n"
-            << "2. Medium dungeon\n"
-            << "3. Hard dungeon\n"
-            << "4. Back\n"
+            << "1. Goblin's Cave\n"
+            << "2. Skeleton's Temple\n"
+            << "3. Frozen Mountain\n"
+            << "4. Fire Mines\n"
+            << "5. Forgotten Forest\n"
+            << "6. Storm Ruins\n"
+            << "7. Back\n"
             << "Choice: ";
         cin >> choice;
 
         switch (choice) {
 
         case 1: {
-            Dungeon easyDungeon("Goblin Cave", "Goblin King", 1, logger_);
-            easyDungeon.showInfo();
-			logger_.log("Player entered the " + easyDungeon.dungeonName + " dungeon.");
+            Dungeon GoblinsCave("Goblin's Cave", "Goblin King", 1, logger_);
+            Enemy GoblinKing("Goblin King", 200, 20, 25, logger_);
+            GoblinsCave.showInfo();
+
+            cout << "Start the battle? (y/n)";
+			char startBattle;
+			cin >> startBattle;
+
+            if(startBattle == 'y' || startBattle == 'Y') {
+                cout << "Starting battle" << "...\n";
+                logger_.log("Player started the battle with " + GoblinKing.getName() + " in the " + GoblinsCave.dungeonName + " dungeon.");
+            } 
+            else if (startBattle == 'n' || startBattle == 'N') {
+                cout << "Returning to dungeon menu...\n";
+			}
             break;
         }
         case 2: {
-            Dungeon mediumDungeon("Skeleton Temple", "Giant Skeleton", 1, logger_);
-            mediumDungeon.showInfo();
-			logger_.log("Player entered the " + mediumDungeon.dungeonName + " dungeon.");
+            Dungeon SkeletonsTemple("Skeleton's Temple", "Giant Skeleton", 2, logger_);
+            Enemy GiantSkeleton("Giant Skeleton", 300, 35, 40, logger_);
+            SkeletonsTemple.showInfo();
+
+            cout << "Start the battle? (y/n)";
+            char startBattle;
+            cin >> startBattle;
+
+            if (startBattle == 'y' || startBattle == 'Y') {
+                cout << "Starting battle" << "...\n";
+                logger_.log("Player started the battle with " + GiantSkeleton.getName() + " in the " + SkeletonsTemple.dungeonName + " dungeon.");
+            }
+            else if (startBattle == 'n' || startBattle == 'N') {
+                cout << "Returning to dungeon menu...\n";
+            }
             break;
         }
         case 3: {
-            Dungeon hardDungeon("Frozen Mountain", "Yety", 1, logger_);
-            hardDungeon.showInfo();
-			logger_.log("Player entered the " + hardDungeon.dungeonName + " dungeon.");
+            Dungeon FrozenMountain("Frozen Mountain", "Yety", 3, logger_);
+            Enemy Yety("Yety", 500, 65, 75, logger_);
+            FrozenMountain.showInfo();
+
+            cout << "Start the battle? (y/n)";
+            char startBattle;
+            cin >> startBattle;
+
+            if (startBattle == 'y' || startBattle == 'Y') {
+                cout << "Starting battle" << "...\n";
+                logger_.log("Player started the battle with " + Yety.getName() + " in the " + FrozenMountain.dungeonName + " dungeon.");
+            }
+            else if (startBattle == 'n' || startBattle == 'N') {
+                cout << "Returning to dungeon menu...\n";
+            }
             break;
         }
-        case 4:
-            cout << "\nBack to main menu...\n";
-			logger_.log("Player returned to the main menu from the dungeon menu.");
+        case 4: {
+            Dungeon FireMines("Fire Mines", "Mine Demon", "Ash Golem", 4, logger_);
+            Enemy MineDemon("Mine Demon", 150, 15, 30, logger_);
+            Enemy AshGolem("Ash Golem", 250, 30, 50, logger_);
+            FireMines.showInfo();
+
+            cout << "Start the battle? (y/n)";
+            char startBattle;
+            cin >> startBattle;
+
+            if (startBattle == 'y' || startBattle == 'Y') {
+                cout << "Starting battle"<< "...\n";
+                logger_.log("Player started the battle with " + MineDemon.getName() + " and " + AshGolem.getName() + " in the " + FireMines.dungeonName + " dungeon.");
+            }
+            else if (startBattle == 'n' || startBattle == 'N') {
+                cout << "Returning to dungeon menu...\n";
+            }
             break;
+        }
+        case 5: {
+            Dungeon ForgottenForest("Forgotten Forest", "Rot Wolf", "Forest Ghost", 5, logger_);
+            Enemy RotWolf("Rot Wolf", 300, 40, 35, logger_);
+            Enemy ForestGhost("Forest Ghost", 500, 10, 70, logger_);
+            ForgottenForest.showInfo();
+
+            cout << "Start the battle? (y/n)";
+            char startBattle;
+            cin >> startBattle;
+
+            if (startBattle == 'y' || startBattle == 'Y') {
+                cout << "Starting battle" << "...\n";
+                logger_.log("Player started the battle with " + RotWolf.getName() + " and " + ForestGhost.getName() + " in the " + ForgottenForest.dungeonName + " dungeon.");
+            }
+            else if (startBattle == 'n' || startBattle == 'N') {
+                cout << "Returning to dungeon menu...\n";
+            }
+
+            break;
+        }
+        case 6: {
+            Dungeon StormRuins("Storm Ruins", "Thunder Wraith", "Wind Elemental", 6, logger_);
+            Enemy ThunderWraith("Thunder Wraith", 250, 40, 35, logger_);
+            Enemy WindElemental("Wind Elemental", 350, 20, 80, logger_);
+            StormRuins.showInfo();
+
+            cout << "Start the battle? (y/n)";
+            char startBattle;
+            cin >> startBattle;
+
+            if (startBattle == 'y' || startBattle == 'Y') {
+                cout << "Starting battle" << "...\n";
+                logger_.log("Player started the battle with " + ThunderWraith.getName() + " and " + WindElemental.getName() + " in the " + StormRuins.dungeonName + " dungeon.");
+            }
+            else if (startBattle == 'n' || startBattle == 'N') {
+                cout << "Returning to dungeon menu...\n";
+            }
+            break;
+        }
+        case 7: {
+            cout << "\nBack to main menu...\n";
+            logger_.log("Player returned to the main menu from the dungeon menu.");
+            break;
+        }
 
         default:
             cout << "\nInvalid choice!\n";
         }
-    } while (choice != 4);
+    } while (choice != 7);
 }
--- Project/Project/World/Dungeon.h
+++ Project/Project/World/Dungeon.h
@@ -1,18 +1,19 @@
 #pragma once
 #include <string>
 #include "../Core/Logger.h"
-
-using namespace std;
+#include "Enemy.h"
 
 class Dungeon {
 private:
     string dungeonName;
-    string bossName;
+    Enemy boss1_;
+    Enemy boss2_;
     int level;
     Logger& logger_;
 
 public:
     Dungeon(string dName, string bName, int lvl, Logger& logger);
+    Dungeon(string dName, string b1Name,string b2Name, int lvl, Logger& logger);
     void showInfo();
     void showMenu();
 };
