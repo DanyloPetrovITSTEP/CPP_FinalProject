106d0ea5ac1706d4cbf2d750dfa6d14490b11d1e
lemonn-ops <retryver10@gmail.com>
2026-05-27 19:16:29 +0300

    Enemy fixed polymorphism

--- Project/Project/World/Enemy.cpp
+++ Project/Project/World/Enemy.cpp
@@ -1,92 +1,45 @@
 #include "Enemy.h"
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 Enemy::Enemy(const string& name, int maxHealth, int damage, int gold, Logger& logger)
-    :logger_(logger), Character(name, maxHealth, damage, gold)
+    : Character(name, maxHealth, damage, gold), logger_(logger), ename_(name), is_dead_(false)
 {
-    ename_ = name;
-    maxHealth_ = maxHealth;
-    health_ = maxHealth;
-    damage_ = damage;
-    gold_ = gold;
-
-    if (maxHealth_ < 1)
-    {
-        maxHealth_ = 1;
-        health_ = 1;
-    }
-    if (damage_ < 0)
-    {
-        damage_ = 0;
-    }
-    if (gold_ < 0)
-    {
-        gold_ = 0;
-    }
     updateIsDead();
 }
 
-string Enemy::getName() const
-{
-    return ename_;
-}
-int Enemy::getHealth()  const
-{
-    return health_;
-}
-int Enemy::getMaxHealth()   const
-{
-    return maxHealth_;
-}
-int Enemy::getDamage()  const
-{
-    return damage_;
-}
-int Enemy::getGold()    const
-{
-    return gold_;
-}
-void Enemy::updateIsDead()
-{
-    this->is_dead_ = getHealth() <= 0;
-    if (is_dead_)
-    {
-        cout << this->ename_ << " has died!" << endl;
-        logger_.log(this->ename_ + " has died!");
-    }
+string Enemy::getName() const { return ename_; }
+int Enemy::getHealth()  const { return Character::getHealth(); }
+int Enemy::getMaxHealth()   const { return Character::getMaxHealth(); }
+int Enemy::getDamage()  const { return Character::getDamage(); }
+int Enemy::getGold()    const { return Character::getGold(); }
 
+bool Enemy::isAlive() const { return Character::isAlive(); }
+
+void Enemy::takeDamage(int value) {
+    Character::takeDamage(value);
+    updateIsDead();
 }
-bool Enemy::basicAttack(Character& target)
-{
+
+void Enemy::updateIsDead() {
+    this->is_dead_ = !isAlive();
+    if (is_dead_) {
+        cout << this->ename_ << " has died!" << endl;
+		logger_.log(this->ename_ + " has died.");
+    }
+}
+
+bool Enemy::basicAttack(Character& target) {
     target.takeDamage(getDamage());
     return true;
 }
-bool Enemy::isAlive() const
-{
-    return health_ > 0;
-}
-void Enemy::takeDamage(int value)
-{
-    if (value < 0)
-        return;
 
-    health_ -= value;
-
-    if (health_ < 0)
-        health_ = 0;
-    updateIsDead();
-}
-
-
-
-string Enemy::getClassName() const { return; }
-string Enemy::getBasicAttackName() const { return; }
-string Enemy::getSecondActionName() const { return; }
-string Enemy::getFirstAbilityName() const { return; }
-string Enemy::getSecondAbilityName() const { return; }
-bool Enemy::secondAction(Character& target) { return; }
-bool Enemy::firstAbility(Character& target, vector<Character*>& enemies) { return; }
-bool Enemy::secondAbility(Character& target, vector<Character*>& enemies) { return; }
+string Enemy::getClassName() const { return "Enemy"; }
+string Enemy::getBasicAttackName() const { return "Strike"; }
+string Enemy::getSecondActionName() const { return ; }
+string Enemy::getFirstAbilityName() const { return ; }
+string Enemy::getSecondAbilityName() const { return ; }
+bool Enemy::secondAction(Character&) { return false; }
+bool Enemy::firstAbility(Character&, vector<Character*>&) { return false; }
+bool Enemy::secondAbility(Character&, vector<Character*>&) { return false; }
