b77d4cb93042f5a190838f15faf3b9d13bd0fe70
sw1bexx-code <sw1bexx@gmail.com>
2026-05-28 13:42:23 +0300

    Game.h

--- Project/Project/Core/Game.h
+++ Project/Project/Core/Game.h
@@ -4,6 +4,9 @@
 #include <memory>
 #include "Logger.h"
 #include "../Save/SaveSystem.h"
+#include "../Items/Inventory.h"
+#include "../Items/Weapon.h"
+#include "../Items/Armor.h"
 
 class Character; 
 
@@ -11,14 +14,19 @@ class Game {
 private:
     bool m_isRunning;
     std::unique_ptr<Character> m_player;
+    Inventory m_inventory;
     Logger m_logger;
     SaveSystem m_saveSystem;
+
+    std::shared_ptr<Weapon> m_equippedWeapon;
+    std::shared_ptr<Armor> m_equippedArmor;
 
     void showMainMenu();
     void showGameMenu();
     void startNewGame();
     void continueGame();
     void selectCharacterClass();
+    
     void goToDungeon();
     void goToShop();
     void restAtHome();
